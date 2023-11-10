6bbbdb173f2a42385deb3029b93663a8d8ebb8e6
Damian Heer <damian@heer.one>
2023-11-10 13:49:11 +0100

    initial commit

---
+++ Source/TurboPacker/TurboPackerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TurboPackerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TURBOPACKER_API ATurboPackerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
